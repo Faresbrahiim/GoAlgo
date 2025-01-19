5d003dc8a04784e3cfe2c3fe0865976e385bd613
Faresbrahiim <hmsteven37@gmail.com>
2025-01-19 15:16:42 +0100

    commit

---
+++ reversedTringle.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+	for i := 1; i <= a; i++ {
+		for k := 1; k < i; k++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= 2*(a-i)+1; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
